9bdc4d2066dbaabe53fca736d1b6825d8929de52
AlbertZaqaryan <itm.albert.zaqaryan@gmail.com>
2023-07-07 19:26:56 +0400

    myfile

---
+++ home1.py
@@ -0,0 +1,8 @@
+# mylist = [7, 4, 5, 8, 9, 6, 1, 1, 4, 5]
+# for i in mylist.copy():
+#     if mylist.count(i) > 1:
+#         mylist.remove(i)
+# print(mylist)
+
+# name = input('Enter name: ')
+# print(f'Hello {name}')
